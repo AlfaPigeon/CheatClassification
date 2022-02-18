b3bf16ec63aa4ad20e55d53e1474b8b4c2fb22c3
kemalbayik <kemalbayikk@gmail.com>
2022-02-18 19:16:48 +0300

    server commit

---
+++ server/src/routes/meeting.routes.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { startMeeting, checkMeetingExists } from '../lib/meeting-cache';
+
+const router = Router();
+interface StartMeetingRequestBody {
+  [key: string]: string;
+}
+interface StartMeetingResponseBody {
+  [meetingId: string]: string;
+}
+router.post<null, StartMeetingResponseBody, StartMeetingRequestBody>('/start', (req, res) => {
+  const { name, userId } = req.body;
+  const meetingId = startMeeting({ name, userId });
+  res.send({ meetingId });
+});
+
+router.get('/join', (req, res) => {
+  console.log("heyy");
+  const { meetingId } = req.query;
+  try {
+    const meeting = checkMeetingExists(meetingId as string);
+    console.log(meeting);
+    res.status(200).send(meeting);
+  } catch (error) {
+    res.status(404).send({ message: 'Meeting not found' });
+    console.log(error);
+  }
+});
+
+export default router;
